d113102ada34695a1c828498331fe776f3dbf4e4
Renan <renan.fachin@hotmail.com>
2024-03-14 23:56:55 -0300

    feat: Adiciona toggle para visualização da senha nos inputs de passwor

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,5 @@
-import { Car, ChevronRight, Loader } from 'lucide-react'
-import { useContext } from 'react'
+import { Car, ChevronRight, Eye, EyeOff, Loader } from 'lucide-react'
+import { useContext, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate, useSearchParams } from 'react-router-dom'
@@ -17,6 +17,8 @@ const signInFormSchema = z.object({
 type SignInFormSchema = z.infer<typeof signInFormSchema>
 
 export function Signin() {
+  const [passwordShown, setPasswordShown] = useState(false)
+
   // Recebendo o email por query parameter após o registro
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
@@ -45,10 +47,14 @@ export function Signin() {
     // console.log(data)
   }
 
+  function togglePasswordVisibility() {
+    setPasswordShown(!passwordShown)
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <div className="p-3 md:p-8 ">
+      <div className="p-3 md:p-8">
         <div className="flex flex-col items-center justify-center gap-6 px-4 md:w-[400px] md:px-0">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-xl font-semibold tracking-tight md:text-2xl">
@@ -61,29 +67,42 @@ export function Signin() {
             onSubmit={handleSubmit(handleSignIn)}
           >
             <div className="space-y-1">
-              <label htmlFor="email" className="text-xs font-bold">
+              <label htmlFor="email" className="text-sm font-bold">
                 E-mail
               </label>
               <input
                 type="email"
                 id="email"
                 placeholder="Seu e-mail"
-                className="focus-within:  flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm outline-none placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-primaryapp-800 focus-within:ring-offset-2"
+                className="flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm text-primaryapp-600 outline-none transition-colors placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-highlight-500/20 focus-within:ring-offset-2"
                 {...register('email')}
               />
             </div>
 
             <div className="space-y-1">
-              <label htmlFor="password" className="text-xs font-bold">
+              <label htmlFor="password" className="text-sm font-bold">
                 Senha
               </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Sua senha"
-                className="focus-within:  flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm outline-none placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-primaryapp-800 focus-within:ring-offset-2"
-                {...register('password')}
-              />
+              <div className="relative">
+                <input
+                  type={passwordShown ? 'text' : 'password'}
+                  id="password"
+                  placeholder="Sua senha"
+                  className="flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm text-primaryapp-600 outline-none transition-colors placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-highlight-500/20 focus-within:ring-offset-2"
+                  {...register('password')}
+                />
+                {!passwordShown ? (
+                  <Eye
+                    className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-primaryapp-300 hover:text-highlight-300/20"
+                    onClick={togglePasswordVisibility}
+                  />
+                ) : (
+                  <EyeOff
+                    className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-primaryapp-300 hover:text-highlight-300/20"
+                    onClick={togglePasswordVisibility}
+                  />
+                )}
+              </div>
             </div>
 
             <Button
@@ -103,7 +122,7 @@ export function Signin() {
 
           <Link
             to="/sign-up"
-            className="group mt-5 flex items-center justify-between rounded-md border border-highlight-700/10 bg-highlight-500/5 px-3 py-2 hover:border-highlight-500 hover:brightness-125 md:px-6 md:py-4"
+            className="group mt-5 flex w-full items-center justify-between rounded-md border border-highlight-700/10 bg-highlight-500/5 px-3 py-2 hover:border-highlight-500 hover:brightness-125 md:px-6 md:py-4"
           >
             <div className="flex items-center gap-4">
               <Car className="size-4 text-highlight-300 md:size-6" />
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,4 +1,5 @@
-import { Car, ChevronRight, Loader } from 'lucide-react'
+import { Car, ChevronRight, Eye, EyeOff, Loader } from 'lucide-react'
+import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
@@ -17,6 +18,8 @@ const signUpFormSchema = z.object({
 type SignUpFormSchema = z.infer<typeof signUpFormSchema>
 
 export function SignUp() {
+  const [passwordShown, setPasswordShown] = useState(false)
+
   const navigate = useNavigate()
 
   const {
@@ -47,6 +50,10 @@ export function SignUp() {
     }
   }
 
+  function togglePasswordVisibility() {
+    setPasswordShown(!passwordShown)
+  }
+
   return (
     <>
       <Helmet title="Cadastro" />
@@ -59,46 +66,59 @@ export function SignUp() {
           </div>
 
           <form
-            className="space-y-4 border-b pb-5"
+            className="w-full space-y-4 border-b pb-5"
             onSubmit={handleSubmit(handleSignUp)}
           >
             <div className="space-y-1">
-              <label htmlFor="email" className="text-xs font-bold">
+              <label htmlFor="email" className="text-sm font-bold">
                 Nome
               </label>
               <input
                 type="username"
                 id="username"
                 placeholder="Seu nome"
-                className="focus-within:  flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm outline-none placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-primaryapp-800 focus-within:ring-offset-2"
+                className="flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm text-primaryapp-600 outline-none transition-colors placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-highlight-500/20 focus-within:ring-offset-2"
                 {...register('username')}
               />
             </div>
 
             <div className="space-y-1">
-              <label htmlFor="email" className="text-xs font-bold">
+              <label htmlFor="email" className="text-sm font-bold">
                 E-mail
               </label>
               <input
                 type="email"
                 id="email"
                 placeholder="Seu e-mail"
-                className="focus-within:  flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm outline-none placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-primaryapp-800 focus-within:ring-offset-2"
+                className="flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm text-primaryapp-600 outline-none transition-colors placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-highlight-500/20 focus-within:ring-offset-2"
                 {...register('email')}
               />
             </div>
 
             <div className="space-y-1">
-              <label htmlFor="password" className="text-xs font-bold">
+              <label htmlFor="password" className="text-sm font-bold">
                 Senha
               </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Sua senha deve ter no mínimo 6 caracteres"
-                className="focus-within:  flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm outline-none placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-primaryapp-800 focus-within:ring-offset-2"
-                {...register('password')}
-              />
+              <div className="relative">
+                <input
+                  type={passwordShown ? 'text' : 'password'}
+                  id="password"
+                  placeholder="Sua senha deve ter no mínimo 6 caracteres"
+                  className="flex h-10 w-full rounded-md border border-input bg-primaryapp-200/20 px-3 py-2 text-sm text-primaryapp-600 outline-none transition-colors placeholder:text-primaryapp-300 focus-within:ring-2 focus-within:ring-highlight-500/20 focus-within:ring-offset-2"
+                  {...register('password')}
+                />
+                {!passwordShown ? (
+                  <Eye
+                    className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-primaryapp-300 hover:text-highlight-300/20"
+                    onClick={togglePasswordVisibility}
+                  />
+                ) : (
+                  <EyeOff
+                    className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-primaryapp-300 hover:text-highlight-300/20"
+                    onClick={togglePasswordVisibility}
+                  />
+                )}
+              </div>
             </div>
 
             <Button
@@ -118,7 +138,7 @@ export function SignUp() {
 
           <Link
             to="/sign-in"
-            className="group mt-5 flex items-center justify-between rounded-md border border-highlight-700/10 bg-highlight-500/5 px-6 py-4 hover:border-highlight-500 hover:brightness-125"
+            className="group mt-5 flex w-full items-center justify-between rounded-md border border-highlight-700/10 bg-highlight-500/5 px-6 py-4 hover:border-highlight-500 hover:brightness-125"
           >
             <div className="flex items-center gap-4">
               <Car className="size-4 text-highlight-300 md:size-6" />
